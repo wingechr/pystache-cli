fdf0e843ced59d24c6530ad94766340d0f820297
Christian Winger <c@wingechr.de>
2021-03-06 18:51:17 +0100

    setup

--- pystache_cli/__init__.py
+++ pystache_cli/__init__.py
@@ -1,7 +1,7 @@
 """
 Extended command line client for pystache
 """
-__version__ = "0.0.1"
+__version__ = "0.0.5"
 __author__ = "Christian Winger"
 __email__ = "c@wingechr.de"
 __url__ = "https://github.com/wingechr/pystache-cli"
--- setup.py
+++ setup.py
@@ -3,17 +3,29 @@ from setuptools import setup
 import pystache_cli as pkg
 
 if __name__ == "__main__":
+
+    with open("README.md", encoding="utf-8") as file:
+        long_description = file.read()
+
     setup(
         packages=[pkg.__name__],
-        install_requires=["pystache"],
+        install_requires=[
+            "pystache"
+        ],
         name=pkg.__name__,
-        description=pkg.__doc__,
+        description=pkg.__doc__.replace('\n', ' '),  # should be one line
+        long_description=long_description,
+        long_description_content_type="text/markdown",  # text/markdown or text/x-rst or text/plain
         version=pkg.__version__,
         author=pkg.__author__,
         author_email=pkg.__email__,
         url=pkg.__url__,
+        project_urls={
+            "Bug Tracker": pkg.__url__,
+        },
         classifiers=[
             "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
         ],
         entry_points={
