a2fdd4d33f6ed3a1acf8032635e75bd23d0856ab
Christian Winger <c@wingechr.de>
2021-05-12 11:49:10 +0200

    ignore import errors during setup

--- pystache_cli/__init__.py
+++ pystache_cli/__init__.py
@@ -1,10 +1,12 @@
 """
 Extended command line client for pystache
 """
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __author__ = "Christian Winger"
 __email__ = "c@wingechr.de"
 __url__ = "https://github.com/wingechr/pystache-cli"
 
-
-from .pystache_cli import render
+try:
+    from .pystache_cli import render
+except ModuleNotFoundError: # during setup
+    pass
